45252e68b85b94888cf085b07dc62aaf23043efb
Eric Hopkins <eric.on.tech@gmail.com>
2015-11-07 22:19:14 -0500

    More :fish: config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,3 +6,19 @@ set __fish_git_prompt_color_cleanstate green
 set __fish_git_prompt_color_stagedstate white
 set __fish_git_prompt_color_upstream_ahead ffb90f
 set __fish_git_prompt_color_upstream_behind blue
+
+# A bunch of junk environmental variables for testing
+set -x email_address 'junk@junk.com'
+set -x PSQL_DEV 'aLocalPassword'
+set -x gmail_username 'junk@junk.com'
+set -x gmail_password 'junkPassword'
+set -x DEV_KEY = 'abc12abc12abc12abc12abc12abc12abc12abc1233333333'
+set -x PRD_KEY = 'abc12abc12abc12abc12abc12abc12abc12abc1233333333'
+
+alias API='~/MekManager/mekmanager_api'
+alias CLIENT='~/MekManager/mekmanager_client'
+alias FISH_DIR='~/.config/fish'
+alias GITHUB='~/GitHub'
+
+alias vi nvim
+alias vim nvim
