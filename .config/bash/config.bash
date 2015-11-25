1e10528eed68cb73367333ecd2df3e4489b5ec3a
Eric Hopkins <eric.on.tech@gmail.com>
2015-11-25 17:51:18 -0500

    Mass Effect joke

--- .config/bash/config.bash
+++ .config/bash/config.bash
@@ -95,7 +95,11 @@ if [ -f "$(brew --prefix bash-git-prompt)/share/gitprompt.sh" ]; then
   GIT_PS1_SHOWCOLORHITS=1
   GIT_PS1_STATESEPARATOR="|"
 
-  GIT_PROMPT_START="\e[0;33m$(whoami)\e[0m at \e[0;35m$(hostname | cut -d . -f 1)\e[0m in \e[0;32m\w\e[0m"
+  if [ $USER == 'root' ]; then
+    GIT_PROMPT_START="\e[1;31mASSUMING DIRECT CONTROL\e[0m"
+  else
+    GIT_PROMPT_START="\e[0;33m$(whoami)\e[0m at \e[0;35m$(hostname | cut -d . -f 1)\e[0m in \e[0;32m\w\e[0m"
+  fi
   GIT_PROMPT_END="\n> "
 fi
 # =============================================================================
