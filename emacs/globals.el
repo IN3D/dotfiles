e0afc557d5e1c4b7f272121d0da485a651e8b11f
Eric Hopkins <eric.on.tech@gmail.com>
2017-01-05 17:15:35 -0500

    Remove emacs config (again)

--- emacs/globals.el
+++
@@ -1,5 +0,0 @@
-;; Global stuff
-(global-wakatime-mode)
-(autopair-global-mode)
-(add-hook 'prog-mode-hook #'editorconfig-mode)
-(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
--- emacs/junk-drawer.el
+++
@@ -1,9 +0,0 @@
-;; Odds and ends, should find themselves into a more concrete file someday
-
-;; All of this is customization that is either too small to deserve it's own
-;; file, or I haven't found a good home for it yet.
-;; Personal Rule of Thumb: If it's going to live in the junk drawer, it should
-;; be well documented so it can be refactored out if/when the time comes.
-
-(setq fci-rule-width 1)
-(setq fci-rule-color "#D0BF8F")
