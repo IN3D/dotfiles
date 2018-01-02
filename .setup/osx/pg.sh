fcd9dc96950d0cb9ca0349d7da21e911ffdc99c8
Eric Hopkins <eric.on.tech@gmail.com>
2018-01-02 17:39:26 -0500

    Delete old Postgres setup file
    This seems like something I wrote before there were Homebrew Services

--- .setup/osx/pg.sh
+++
@@ -1,8 +0,0 @@
-# setup postgresql on OS X
-if [ `uname` == "Darwin" ]; then
-  initdb /usr/local/var/postgres
-  mkdir -p ~/Library/LaunchAgents
-  ln -svf /usr/local/opt/postgresql/*.plist ~/Library/LaunchAgents
-  launchctl load ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist
-  createdb `whoami`
-fi
