78396de6320b23d1eafbe53a2c7a0b548ce0cffb
Eric Hopkins <eric.on.tech@gmail.com>
2018-05-17 16:47:43 -0400

    Put screenshots into their own folder when taken

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -21,6 +21,6 @@ main = do
       } `additionalKeys`
       [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
       , ((mod4Mask .|. shiftMask, xK_r), renameWorkspace defaultXPConfig)
-      , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
-      , ((0, xK_Print), spawn "scrot")
+      , ((controlMask, xK_Print), spawn "sleep 0.2; cd ~/Pictures/scrots; scrot -s")
+      , ((0, xK_Print), spawn "cd ~/Pictures/scrots; scrot")
       ]
