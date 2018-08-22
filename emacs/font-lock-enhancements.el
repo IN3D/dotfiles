d4231a31a350fbce15543358625de607136e106e
Eric Hopkins <eric.on.tech@gmail.com>
2018-08-22 00:11:05 -0400

    Add better font-locks for TypeScript
    typescript-mode comes with some underwhelming syntax
    highlighting. While I find VS Code to highlights too much stuff, and
    so nothing stands out, I want more than the minimum. I still invoke
    this personally because I'm not 100% confident on it, so it's still a
    bit of a WIP.

--- emacs/font-lock-enhancements.el
+++ emacs/font-lock-enhancements.el
@@ -5,11 +5,21 @@
 ;; (defface ejh/class-name-bright
 ;;   '((t (:inherit dired-marked-face))))
 
+;; "\\(?:^\\s *\\|[[{(,]\\s *\\|\\sw\\s +\\)\\(\\(\\sw\\|_\\)+:\\)[^:]"
+;;   (1
+;;    (progn
+;;      (forward-char -1)
+;;      font-lock-constant-face))
+
 (let ((case-fold-search t))
   (font-lock-add-keywords
    'typescript-mode
-   '(("\s?\\(@[A-Za-z]*\\)"    1 font-lock-warning-face)
-     ("\\(\s\\|\\.\\)\\([Ab-Z][a-zA-Z]*\\)" 2 font-lock-type-face)))
+   '(("\s?\\(@[A-Za-z]*\\)"                1 font-lock-warning-face)
+     ("\\(\s\\|\\.\\)\\([A-Z][a-zA-Z]*\\)" 2 font-lock-type-face)
+     ("\\(?:^\\s *\\|[[{(,]\\s *\\|\\sw\\s +\\)\\(\\(\\sw\\|_\\)+:\\)[^\\??:]"
+      1 (progn
+          (forward-char -1)
+          font-lock-constant-face))))
   (font-lock-add-keywords
    'coffee-mode
    '(("\\(\s\\|\\.\\)\\([A-Z][a-zA-Z]*\\)" 2 dired-marked-face)))
