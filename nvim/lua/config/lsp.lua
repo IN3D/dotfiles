61e50ad00cb9d9af71b5cfd17a744cf72ee8c3b2
Eric Hopkins <eric.on.tech@gmail.com>
2025-04-12 15:38:50 -0400

    Update nvim diagnostics config

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -46,6 +46,20 @@ lspconfig.solargraph.setup({
       })
     end
 
+    local diagnostic_config = {
+      virtual_text = false,
+      signs = true,
+      update_in_insert = false,
+      float = {
+        border = 'rounded',
+        source = 'always',
+        header = '',
+        prefix = '',
+        focusable = false
+      },
+    }
+    vim.diagnostic.config(diagnostic_config)
+
     -- Set keymaps
     local opts = { noremap=true, silent=true, buffer=bufnr }
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
@@ -54,5 +68,11 @@ lspconfig.solargraph.setup({
     vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
     vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    -- Show diagnostics in floating window
+    vim.keymap.set('n', '<leader>ee', vim.diagnostic.open_float, opts)
+    -- navigate between diagnostics
+    vim.keymap.set('n', '<leader>ep', vim.diagnostic.goto_prev, opts)
+    vim.keymap.set('n', '<leader>en', vim.diagnostic.goto_next, opts)
+    vim.keymap.set('n', '<leader>el', vim.diagnostic.setloclist, opts)
   end
 })
