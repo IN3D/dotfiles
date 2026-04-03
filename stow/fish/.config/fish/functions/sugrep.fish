4c020e2d8d85ad9e92f2c8014bbb211f7584c449
Eric Hopkins <eric.on.tech@gmail.com>
2026-04-03 19:10:06 -0400

    Small doc update

--- stow/fish/.config/fish/functions/sugrep.fish
+++ stow/fish/.config/fish/functions/sugrep.fish
@@ -1,5 +1,5 @@
-# Super grep: ripgrep when installed, else grep -R (same idea: -n -H -C 5, skip VCS dirs)
-function sugrep
+# I think I got the original version of this from Kevin in 2016?
+function sugrep --description "\"Super grep\" show header with 5 numbered lines of context"
   if command_exists rg
     rg -C 5 -H --hidden \
       --glob '!**/.git/**' --glob '!**/.hg/**' --glob '!**/.svn/**' --glob '!**/CVS/**' \
