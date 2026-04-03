916239c2a08faf4501ebffe9d85e88943e76332c
Eric Hopkins <eric.on.tech@gmail.com>
2026-04-03 15:16:18 -0400

    Git config updates

---
+++ stow/fish/.config/fish/functions/git-push-all.fish
@@ -0,0 +1,10 @@
+function git-push-all --description "Push current branch to all git remotes"
+    if not git rev-parse --is-inside-work-tree &>/dev/null
+        echo "Not a git directory"
+        return 1
+    end
+
+    for remote in (git remote)
+        git push $remote
+    end
+end
