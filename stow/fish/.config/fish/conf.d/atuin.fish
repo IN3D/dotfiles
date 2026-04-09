b1376d165e5a72b744e2cf931da9a63cd278dfb1
Eric Hopkins <eric.on.tech@gmail.com>
2026-04-09 02:02:08 -0400

    Add atuin

---
+++ stow/fish/.config/fish/conf.d/atuin.fish
@@ -0,0 +1,3 @@
+if command_exists atuin
+  atuin init fish --disable-up-arrow | source
+end
--- stow/fish/.config/fish/conf.d/fzf.fish
+++ stow/fish/.config/fish/conf.d/fzf.fish
@@ -4,4 +4,11 @@ if command_exists fzf
 
   # Use ctrl-t for command history correctly
   export FZF_DEFAULT_COMMAND="command find -L \$dir -type f 2> /dev/null | sed '1d; s#^\./##'"
+
+  # because fish config files load in alphabetical order. This file will always
+  # load after the `atuin.fish` file, and overwrite atuin's ctrl+r keybinding.
+  if command_exists atuin
+    bind ctrl-r _atuin_search
+    bind -M insert ctrl-r _atuin_search
+  end
 end
