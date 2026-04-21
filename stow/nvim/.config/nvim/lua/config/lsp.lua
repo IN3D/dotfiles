3e6f3c83f4f5dca2003835bc239630564882d02a
Eric Hopkins <eric.on.tech@gmail.com>
2026-04-20 23:28:30 -0400

    Add keymappings for Odin LSP

--- stow/nvim/.config/nvim/lua/config/lsp.lua
+++ stow/nvim/.config/nvim/lua/config/lsp.lua
@@ -93,3 +93,21 @@ if solargraph_cmd then
   vim.lsp.enable('solargraph')
 end
 
+vim.lsp.config('ols', {
+  -- Set keymaps
+  on_attach = function(client, bufnr)
+    local opts = { noremap=true, silent=true, buffer=bufnr }
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    -- Show diagnostics in floating window
+    vim.keymap.set('n', '<leader>ee', vim.diagnostic.open_float, opts)
+    -- navigate between diagnostics
+    vim.keymap.set('n', '<leader>ep', vim.diagnostic.goto_prev, opts)
+    vim.keymap.set('n', '<leader>en', vim.diagnostic.goto_next, opts)
+    vim.keymap.set('n', '<leader>el', vim.diagnostic.setloclist, opts)
+  end
+})
